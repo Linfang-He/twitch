31389c23715caa132c070d6b488e546952cfb661
Linfang He <40910119+Linfang-He@users.noreply.github.com>
2020-04-29 01:53:54 +0800

    build graph
    build graph with all the relations

--- build_graph.py
+++ build_graph.py
@@ -1,13 +1,17 @@
 import dgl
 import pandas as pd
 import pickle
+import torch
 from builder import PandasGraphBuilder
 
 channels = pd.read_csv('channels.csv')
 users = pd.read_csv('users.csv')
 games = pd.read_csv('games.csv')
+games_play = pd.read_csv('games_play.csv')
+
+play2game = pd.read_csv('play2game.csv')
 comments = pd.read_csv('comments.csv')
-# subscribes = pd.read_csv('train_truth.csv')
+subscribes = pd.read_csv('train_truth.csv')
 
 
 # Remove meaningless columns
@@ -17,38 +21,74 @@ users.columns = ['_', 'user']
 users = users.drop(columns = '_', axis=1)
 games.columns = ['_', 'game']
 games = games.drop(columns = '_', axis=1)
-# comments.columns = ['_', 'channel', 'user', 'timestamp', 'game', 'comment']
-# comments = games.drop(columns = '_', axis=1)
+games_play.columns = ['_', 'game_play']
+games_play = games_play.drop(columns = '_', axis=1)
+
+play2game.columns = ['_', 'game_play', 'game']
+play2game = play2game.drop(columns = '_', axis=1)
+comments.columns = ['_', 'channel', 'user', 'timestamp', 'message', 'game_play']
+comments = comments.drop(columns = ['_', 'message'], axis=1)
+
 print("Finish reading csv files")
 
 
-# Build user-comment-game graph
-builder_comment = PandasGraphBuilder()
-builder_comment.add_entities(users, 'user', 'user')
-builder_comment.add_entities(games, 'game', 'game')
-builder_comment.add_binary_relations(comments, 'user', 'game', 'comments')
-# builder_comment.add_binary_relations(comments, 'game', 'user', 'commented-by')
-g_comment = builder_comment.build()
+# Build trirelation graph
+builder = PandasGraphBuilder()
+builder.add_entities(users, 'user', 'user')
+builder.add_entities(games, 'game', 'game')
+builder.add_entities(channels, 'channel', 'channel')
+builder.add_entities(games_play, 'game_play', 'game_play')
+
+builder.add_binary_relations(play2game, 'game_play', 'game', 'corresponds')
+# builder.add_binary_relations(play2game, 'game_play', 'game', 'corresponded')
+builder.add_binary_relations(comments, 'user', 'game_play', 'comments')
+# builder.add_binary_relations(comments, 'game', 'user', 'commented-by')
+builder.add_binary_relations(comments, 'user', 'channel', 'watches')
+# builder.add_binary_relations(comments, 'user', 'channel', 'watched-by')
+builder.add_binary_relations(comments, 'channel', 'game_play', 'contains')
+# builder.add_binary_relations(comments, 'game', 'channel', 'contained-by')
+builder.add_binary_relations(subscribes, 'user', 'channel', 'subscribes')
+# builder.add_binary_relations(subscribes, 'channel', 'user', 'subscribed-by')
+g = builder.build()
 
 # Assign features
-g_comment.edges['comments'].data['timestamp'] = torch.LongTensor(comments['timestamp'].values)
+g.edges['comments'].data['timestamp'] = torch.LongTensor(comments['timestamp'].values)
+# g.edges['commented-by'].data['timestamp'] = torch.LongTensor(comments['timestamp'].values)
+
+with open('g_comm_subs.pkl', 'wb') as f:
+	pickle.dump(g, f)
+print("Finish building the graph")
+
+
+'''Three subgraphs'''
+# Build user-comment-game graph
+# builder_comment = PandasGraphBuilder()
+# builder_comment.add_entities(users, 'user', 'user')
+# builder_comment.add_entities(games, 'game', 'game')
+# builder_comment.add_binary_relations(comments, 'user', 'game', 'comments')
+# builder_comment.add_binary_relations(comments, 'game', 'user', 'commented-by')
+# g_comment = builder_comment.build()
+
+# Assign features
+# g_comment.edges['comments'].data['timestamp'] = torch.LongTensor(comments['timestamp'].values)
 # g_comment.edges['commented-by'].data['timestamp'] = torch.LongTensor(comments['timestamp'].values)
 
-with open('g_comment.pkl', 'wb') as f:
-	pickle.dump(g_comment, f)
-print("Finish building user-comment-game graph")
+# with open('g_comment.pkl', 'wb') as f:
+# 	pickle.dump(g_comment, f)
+# print("Finish building user-comment-game graph")
+
 
 # Build channel-contain-game graph
-builder_contain = PandasGraphBuilder()
-builder_contain.add_entities(channels, 'channel', 'channel')
-builder_contain.add_entities(games, 'game', 'game')
-builder_contain.add_binary_relations(comments, 'channel', 'game', 'contains')
+# builder_contain = PandasGraphBuilder()
+# builder_contain.add_entities(channels, 'channel', 'channel')
+# builder_contain.add_entities(games, 'game', 'game')
+# builder_contain.add_binary_relations(comments, 'channel', 'game', 'contains')
 # builder_contain.add_binary_relations(comments, 'game', 'channel', 'contained-by')
-g_contain = builder_contain.build()
+# g_contain = builder_contain.build()
 
-with open('g_contain.pkl', 'wb') as f:
-	pickle.dump(g_contain, f)
-print("Finish building channel-contain-game graph")
+# with open('g_contain.pkl', 'wb') as f:
+# 	pickle.dump(g_contain, f)
+# print("Finish building channel-contain-game graph")
 
 
 # Build user-subscribe-channel graph
@@ -56,7 +96,7 @@ print("Finish building channel-contain-game graph")
 # builder_subscribe.add_entities(channels, 'channel', 'channel')
 # builder_subscribe.add_entities(users, 'user', 'user')
 # builder_subscribe.add_binary_relations(subscribes, 'user', 'channel', 'subscribes')
-# # builder_subscribe.add_binary_relations(subscribe, 'channel', 'user', 'subscribed-by')
+# builder_subscribe.add_binary_relations(subscribes, 'channel', 'user', 'subscribed-by')
 # g_subscribe = builder_subscribe.build()
 
 # with open('g_subscribe.pkl', 'wb') as f:
--- twitch_preprocessing.py
+++ twitch_preprocessing.py
@@ -1,8 +1,8 @@
 import json
 import pandas as pd
 
-channels, users, games = [], [], []
-comments = []
+channels, users, games, games_play = [], [], [], []
+comments, play2game = [], []
 
 # lines = []
 with open('train.json', 'r') as f:
@@ -16,24 +16,35 @@ with open('train.json', 'r') as f:
 				'channel': record['c'],
 				'user': record['u'],
 				'timestamp': ms['t'],
-				'game': ms['g'],
 				'comment': ms['m'],
+				'game_play': record['c'] + '_' + ms['g'],
 				})
+			play2game.append({
+				'game_play': record['c'] + '_' + ms['g'],
+				'game': ms['g'],
+				})
+			games_play.append(record['c'] + '_' + ms['g'])
 			games.append(ms['g'])
 
 channels = pd.DataFrame(list(set(channels)))
 users = pd.DataFrame(list(set(users)))
 games = pd.DataFrame(list(set(games)))
-comments = pd.DataFrame(comments).astype({'channel': 'category', 'user': 'category', 'game': 'category'})
+games_play = pd.DataFrame(list(set(games_play)))
+play2game = pd.DataFrame(play2game).astype('category')
+comments = pd.DataFrame(comments).astype({'channel': 'category', 'user': 'category', 'game_play': 'category'})
 
 channels.to_csv('channels.csv')
 users.to_csv('users.csv')
 games.to_csv('games.csv')
+games_play.to_csv('games_play.csv')
+play2game.to_csv('play2game.csv')
 comments.to_csv('comments.csv')
 
 print("Number of channels:", channels.shape[0])  # 146537
 print("Number of users:", users.shape[0])  # 7923774
 print("Number of games:", games.shape[0])  # 7576
+print("Number of games_play", games_play.shape[0])  # 301815
+print("Number of play2game", play2game.shape[0])  # 410686442
 print("Number of records:", comments.shape[0])  # 410686442
 print("Finish processing train.json")
 
